83f403870b73d35a9002fb79ff0a5f054ccd3f36
Pavel Vashkel <jgugn1@gmail.com>
2023-12-28 00:33:47 +0100

    feat: render simple ui

--- composeApp/src/commonMain/kotlin/ui/FeedSceneContent.kt
+++ composeApp/src/commonMain/kotlin/ui/FeedSceneContent.kt
@@ -1,12 +1,36 @@
 package ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import model.Item
 
 @Composable
 fun FeedScreenContent(
     feedItems: List<Item> = emptyList(),
 ) {
-    Text("Feed was here. ${feedItems.size} items total")
+    LazyColumn {
+        items(
+            items = feedItems,
+            key = Item::id,
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(
+                        horizontal = 16.dp,
+                        vertical = 8.dp,
+                    )
+            ) {
+                Text(text = it.title)
+                Text(text = it.pubDate)
+                Text(text = it.link)
+                Text(text = it.commentsLink)
+            }
+        }
+    }
 }
